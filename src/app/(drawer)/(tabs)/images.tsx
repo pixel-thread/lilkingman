207880b536817c2d06499809783f64e0fc29fcde
JyrwaJR <jyrwaboys@gmail.com>
2025-08-17 18:51:20 +0530

    add container

--- src/app/(drawer)/(tabs)/images.tsx
+++ src/app/(drawer)/(tabs)/images.tsx
@@ -1,6 +1,10 @@
 import { Container } from '~/src/components/common/Container';
 import { PhotoScreen } from '~/src/components/screen/photo';
 
-export default function Home() {
-  return <PhotoScreen />;
+export default function page() {
+  return (
+    <Container>
+      <PhotoScreen />
+    </Container>
+  );
 }
